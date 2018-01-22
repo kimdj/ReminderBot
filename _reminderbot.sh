73d1bd513d886136c799223d72a1387a3dff272f
David Kim <kim.david.j@gmail.com>
2018-01-22 07:21:43 -0800

    Added LICENSE.md (MIT license); changed all $var ==> ${var}

--- _reminderbot.sh
+++ _reminderbot.sh
@@ -21,20 +21,20 @@ KEY="$(cat ./config.txt)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 nanos=1000000000
-interval=$(( $nanos * 50 / 100 ))
+interval=$(( ${nanos} * 50 / 100 ))
 declare -i prevdate
 prevdate=0
 
 function send {
     while read -r line; do
-        newdate=`date +%s%N`
-        if [ $prevdate -gt $newdate ] ; then
-            sleep `bc -l <<< "($prevdate - $newdate) / $nanos"`
-            newdate=`date +%s%N`
+        newdate="$(date +%s%N)"
+        if [ ${prevdate} -gt ${newdate} ] ; then
+            sleep $(bc -l <<< "(${prevdate} - ${newdate}) / ${nanos}")
+            newdate="$(date +%s%N)"
         fi
-        prevdate=$newdate+$interval
-        echo "-> $1"
-        echo "$line" >> ${BOT_NICK}.io
+        prevdate=${newdate}+${interval}
+        echo "-> ${1}"
+        echo "${line}" >> ${BOT_NICK}.io
     done <<< "$1"
 }
 
@@ -48,14 +48,18 @@ function signalSubroutine {
             chan=$(echo ${line} | sed -r 's/^.*: ([ a-zA-Z0-9:]*), ([^,]*), .*/\2/')
             nick=$(echo ${line} | sed -r 's/^.*: ([ a-zA-Z0-9:]*), ([^,]*), (.*), .*/\3/')
             task=$(echo ${line} | sed -r 's/^.*: ([ a-zA-Z0-9:]*), ([^,]*), (.*), (.*.)/\4/')
-            echo "line ========> ${line}"
+
+            echo "==> tmp file detected; processing reminder ..."
+            echo "line ===============> ${line}"
             echo "uuid ===============> ${uuid}"
             echo "time_sched =========> ${time_sched}"
             echo "task ===============> ${task}"
             echo "nick ===============> ${nick}"
             echo "chan ===============> ${chan}"
-            # send "PRIVMSG ${chan} :line ==> ${line}"
-            send "PRIVMSG _reminderbot :!signal ${task} ~ ${time_sched}) ~ ${chan} ~ ${nick}"
+
+            payload="!signal ${task} ~ ${time_sched}) ~ ${chan} ~ ${nick}"
+
+            send "PRIVMSG _reminderbot :${payload}"
             crontab -l | grep -v "${uuid}" | crontab -              # Remove the cronjob by grep-ing out the specified cronjob uuid.
         done < tasks/tmp
 
@@ -68,7 +72,7 @@ function signalSubroutine {
 function cmdSubroutine {
     if [ -e commands/cmd ] ; then                       # if a cmd file exists, run the cmd
         while read line ; do
-            send "$line"
+            send "${line}"
         done < commands/cmd
         rm commands/cmd
     fi
@@ -89,34 +93,34 @@ tail -f ${BOT_NICK}.io | openssl s_client -connect irc.cat.pdx.edu:6697 | while 
     #     exec 2> >(tee -a ${LOG_FILE_2} >&2)
     # fi
 
-    if [[ -z $started ]] ; then
-        send "NICK $BOT_NICK"
-        send "USER 0 0 0 :$BOT_NICK"
+    if [[ -z ${started} ]] ; then
+        send "NICK ${BOT_NICK}"
+        send "USER 0 0 0 :${BOT_NICK}"
         started="yes"
     fi
 
-    while [ -z "${irc}" ] ; do                          # While loop is used to enable non-blocking I/O (read).
-        read -t 0.5 irc                                 # Time out and return failure if a complete line of input is not read within TIMEOUT seconds.
+    while [ -z "${irc}" ] ; do                              # While loop is used to enable non-blocking I/O (read).
+        read -t 0.5 irc                                     # Time out and return failure if a complete line of input is not read within TIMEOUT seconds.
         if [ "$(echo $?)" == "1" ] ; then irc='' ; fi
 
         signalSubroutine
         cmdSubroutine
     done
 
-    # echo "==> $irc" >> irc-output.log                 # Re-direct incoming internal irc msgs to file.
-    echo "==> $irc"
-    if $(echo "$irc" | cut -d ' ' -f 1 | grep -P "PING" > /dev/null) ; then
+    # echo "==> ${irc}" >> irc-output.log                     # Re-direct incoming internal irc msgs to file.
+    echo "==> ${irc}"
+    if $(echo "${irc}" | cut -d ' ' -f 1 | grep -P "PING" > /dev/null) ; then
         send "PONG"
-    elif $(echo "$irc" | cut -d ' ' -f 2 | grep -P "PRIVMSG" > /dev/null) ; then 
+    elif $(echo "${irc}" | cut -d ' ' -f 2 | grep -P "PRIVMSG" > /dev/null) ; then 
 #:nick!user@host.cat.pdx.edu PRIVMSG #bots :This is what an IRC protocol PRIVMSG looks like!
-        nick="$(echo "$irc" | cut -d ':' -f 2- | cut -d '!' -f 1)"
-        chan="$(echo "$irc" | cut -d ' ' -f 3)"
-        if [ "$chan" = "$BOT_NICK" ] ; then chan="$nick" ; fi 
-        msg="$(echo "$irc" | cut -d ' ' -f 4- | cut -c 2- | tr -d "\r\n")"
-        echo "$(date) | $chan <$nick>: $msg"
-        var="$(echo "$nick" "$chan" "$msg" | ./commands.sh)"
-        if [[ ! -z $var ]] ; then
-            send "$var"
+        nick="$(echo "${irc}" | cut -d ':' -f 2- | cut -d '!' -f 1)"
+        chan="$(echo "${irc}" | cut -d ' ' -f 3)"
+        if [ "${chan}" = "${BOT_NICK}" ] ; then chan="${nick}" ; fi 
+        msg="$(echo "${irc}" | cut -d ' ' -f 4- | cut -c 2- | tr -d "\r\n")"
+        echo "$(date) | ${chan} <${nick}>: ${msg}"
+        var="$(echo "${nick}" "${chan}" "${msg}" | ./commands.sh)"
+        if [[ ! -z ${var} ]] ; then
+            send "${var}"
         fi
     fi
 
